4c7c430e648e7a72283eb0f3d902a9c39fce255e
joshhigg <136753578+joshhigg@users.noreply.github.com>
2023-10-27 13:14:50 -0600

    able to generate circles

---
+++ index.js
@@ -0,0 +1,72 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const examples = './examples'
+const { createDocument } = require('./lib/shapes')
+
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Please enter logo text. (cannot exceed 3 characters)',
+            validate: (input) => {
+                if(input.length > 3) {
+                    return 'Text cannot exceed 3 characters!'
+                }
+                return true
+            }
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'What color should the text be?'
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Which shape should the logo be?',
+            choices: ['circle', 'triangle', 'square']
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'What color should the shape be?'
+        }
+    ])
+    .then((data) => {
+    const filename = path.join(examples, `logo.svg`);
+
+
+    const logoData = {
+        text: data.text,
+        textColor: data.textColor,
+        shape: data.shape,
+        shapeColor: data.shapeColor
+    }
+    console.log(logoData)
+
+
+    // insert the logoData into some svg that has been created.
+
+
+    console.log(logoData.textColor);
+
+    const logoCode = `
+        <svg version="1.1"
+        width="300" height="200"
+        xmlns="http://www.w3.org/2000/svg">
+    
+     <circle cx="150" cy="100" r="80" fill="${logoData.shapeColor}" />
+    
+     <text x="150" y="125" font-size="60" text-anchor="middle" fill="${logoData.textColor}">${logoData.text}</text>
+    
+    </svg>`;
+    console.log(logoCode);
+    
+    fs.writeFile(filename, logoCode, (err) =>
+    err ? console.log(err) : console.log('Success!'));
+    
+});
+
